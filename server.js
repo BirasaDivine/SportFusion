f5887b740da8f8b6f7c0c65da91d3accee8cd5cd
Birasa Divine <b.divine@alustudent.com>
2024-11-29 17:04:15 +0200

    Update server.js FIXED MODULE IMPORTS

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-import express from 'express';
-import cors from 'cors';
-import fetch from 'node-fetch'; // Importing fetch from node-fetch v3.x (ESM)
+// import express from 'express';
+// import cors from 'cors';
+// import fetch from 'node-fetch'; // Importing fetch from node-fetch v3.x (ESM)
+const express = require('express');
+const cors = require('cors');
+const fetch = require('node-fetch');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
